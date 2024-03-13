dba13a7f65bf54a82ac877bef796f22c43358533
othmxnee <161128946+othmxnee@users.noreply.github.com>
2024-03-13 13:57:40 +0100

    s

--- csv_project/csv_app/models.py
+++ csv_project/csv_app/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    matricule = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)  # the password is the same as matricule
+    
+    def __str__(self):
+        return self.full_name
